59d9f0176994b9d65228211f23cf58dcdebd0463
Gwendoline Rodriguez <gwendolinerodriguez@gmail.com>
2017-11-24 12:26:20 +1000

    add pset2

--- pset2/crack/crack.c
+++ pset2/crack/crack.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <string.h>
 
+int MAXLEN = 1;
+
 int initLetterTab(char *tab)
 {
     int i = 0;
@@ -23,57 +25,33 @@ int initLetterTab(char *tab)
     return 0;
 }
 
-int getMaxLen(int a)
-{
-    if (a < 52)
-        return 1;
-    else if (a < (52 * 52))
-        return 2;
-    else if (a < (52 * 52 * 52))
-        return 3;
-    return 4;
-}
-
-int genString(char *tab, char *str, int a, int c)
+int genString(char *tab, char *str, int c, char *hash)
 {
     int i = 0;
-    while ((i < 52) && (c < getMaxLen(a)))
+    while (i < 52)
     {
-        str[c] = tab[(a + i++) % 52];
-        if (i == 52)
-            c++;
-        genString(tab, str, a, c + 1);
+        str[c] = tab[i];
+        if ((c + 1) < MAXLEN)
+            genString(tab, str, (c + 1), hash);
+        else
+        {
+            str[c + 1] = '\0';
+            if (strcmp(hash, crypt(str, "50")) == 0)
+            {
+                printf("%s\n", str);
+                exit(0);
+            }
+        }
+        i++;
     }
-    return 0;
-}
-
-bool decode(char *hash, char *tab)
-{
-    char *tmp = (char *)malloc(4);
-    int attempt = 0;
-    // while (attempt < 520)
-    while (attempt < (52 * 52))
-    {
-        // generateStr(tmp, attempt, tab);
-        genString(tab, tmp, attempt, 0);
-        tmp[getMaxLen(attempt)] = '\0';
-        printf("%s\n", tmp);
-
-        // if (strcmp(tmp, crypt(tmp, "50")) == 0)
-        // {
-        // printf("%s\n",tmp);
-        // free(tmp);
-        // return true;
-        // }
-        attempt++;
-    }
-    free(tmp);
-    return false;
+    return 1;
 }
 
 int main(int ac, char **av)
 {
     char *tab = (char *)malloc(52);
+    char *tmp = (char *)malloc(4);
+
     if (ac != 2)
     {
         printf("Usage: ./crack hash\n");
@@ -81,7 +59,12 @@ int main(int ac, char **av)
         return 2;
     }
     initLetterTab(tab);
-    decode(av[1], tab);
+    while (MAXLEN <= 4)
+    {
+        genString(tab, tmp, 0, av[1]);
+        MAXLEN++;
+    }
     free(tab);
+    free(tmp);
     return 0;
 }
