63d846d8cdb6ebdfec1ee8181253c7d30b0a6d3f
Gwendoline Rodriguez <gwendolinerodriguez@gmail.com>
2017-12-07 16:55:17 +1000

    add mario.py

---
+++ pset6/mario.py
@@ -0,0 +1,45 @@
+from cs50 import *
+
+
+def checkNb(nb):
+    if nb <= 0 or nb >= 24:
+        print("Usage : Please enter a value between 0 and 23 included.")
+        return False
+    return True
+
+
+def drawPyramid(height):
+    i = 0
+    c = 0
+    gap = "  "
+    while i < height:
+        space = height - i
+        while c <= i:
+            while space > 0:
+                print(" ", end='')
+                space -= 1
+            print("#", end='')
+            c += 1
+        print(gap)
+        c = 0
+        while c <= i:
+            print("#", end='')
+            c += 1
+        i += 1
+        print("")
+
+
+def main():
+    isValid = False
+    while not isValid:
+        print("Height : ", end='')
+        height = get_int()
+        isValid = checkNb(height)
+    if height == 0:
+        return 0
+    drawPyramid(height)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
