3255aa1e98d579b60333f1b4b5f1cc4a1433c3a6
Gwendoline Rodriguez <gwendolinerodriguez@gmail.com>
2017-12-08 17:52:32 +1000

    add analyzer.py

--- pset6/sentiments/analyzer.py
+++ pset6/sentiments/analyzer.py
@@ -1,15 +1,35 @@
 import nltk
 
+
 class Analyzer():
     """Implements sentiment analysis."""
+    positiveWords = set()
+    negativeWords = set()
 
     def __init__(self, positives, negatives):
         """Initialize Analyzer."""
+        file = open(positives, "r")
+        for line in file:
+            if not line.startswith(';'):
+                self.positiveWords.add(line.strip(" "))
+        file.close()
 
-        # TODO
+        file = open(negatives, "r")
+        for line in file:
+            if not line.startswith(';'):
+                self.negativeWords.add(line.strip(" "))
+        file.close()
 
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
 
-        # TODO
-        return 0
+        words = text.split()
+        score = 0
+        for word in words:
+            if word.lower() in self.negativeWords:
+                score -= 1
+            elif word.lower() in self.negativeWords:
+                score += 1
+            continue
+
+        return score
